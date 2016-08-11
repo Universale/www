1dd48bb8dde180c7a1d6e99cd77dcec15efb9cbe
Thibaut Broggi <broggi_t@epitech.eu>
2016-08-12 00:54:20 +0200

    Add records description on main page
        Bootstrap's collapsed panels are used instead of standard ul list
        Add bootstrap javascript include in layout

--- src/universale/webroot/categories/records.cr
+++ src/universale/webroot/categories/records.cr
@@ -4,15 +4,15 @@ get "/categories/:name/records" do |env|
   offset = env.params.query["offset"]? || "0"
   search = env.params.query["search"]? || nil
 
-  q1 = "SELECT records.title, records.data FROM records
+  q1 = "SELECT records.title, records.data, records.description FROM records
   INNER JOIN categories ON categories.id = records.category_id
   WHERE categories.name = $1"
   q2 = "ORDER BY created_at DESC OFFSET $2 LIMIT 100;"
 
   records = if search.nil?
-    DB.exec({String, String}, "#{q1} #{q2}", [category_name, offset]).to_hash
+    DB.exec({String, String, String}, "#{q1} #{q2}", [category_name, offset]).to_hash
   else
-    DB.exec({String, String}, "#{q1} AND records.title LIKE '%' || $3 || '%' #{q2}", [category_name, offset, search.to_s]).to_hash
+    DB.exec({String, String, String}, "#{q1} AND records.title LIKE '%' || $3 || '%' #{q2}", [category_name, offset, search.to_s]).to_hash
   end
 
   if env.json?
--- src/universale/webroot/index.cr
+++ src/universale/webroot/index.cr
@@ -1,6 +1,6 @@
 get "/" do
-  recents = DB.exec({String, String}, "SELECT records.title, records.data FROM records ORDER BY created_at DESC LIMIT 10;").to_hash
-  best = DB.exec({String, String}, "SELECT records.title, records.data FROM records ORDER BY id ASC LIMIT 10;").to_hash # TODO
+  recents = DB.exec({String, String, String}, "SELECT records.title, records.data, records.description FROM records ORDER BY created_at DESC LIMIT 10;").to_hash
+  best = DB.exec({String, String, String}, "SELECT records.title, records.data, records.description FROM records ORDER BY id ASC LIMIT 10;").to_hash # TODO
 
   render "src/universale/webroot/index.slang", "src/universale/webroot/layouts/layout.slang"
 end
