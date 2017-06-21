fb962789b680f0c946349aad790b9fedca8423aa
Arthur Poulet <arthur.poulet@mailoo.org>
2017-06-21 19:32:06 +0100

    Begin the ORM (select, update, insert are ready, not the joins)

--- src/universale/webroot/records/update.cr
+++ src/universale/webroot/records/update.cr
@@ -2,9 +2,10 @@ private def record_update(env)
   begin
     id = Int32.new env.params.url["id"]
     data = env.params.body
-    # record = {data["title"], data["data"], data["description"], data["category_id"]}
-    # success = Universale::DB.exec("UPDATE records SET title = $1, data = $2, description = $3, category_id = $4) WHERE id = $5", *record, id) rescue false
-    record = {title: data["title"], data: data["data"], description: data["description"], category_id: data["category_id"]}
+    category_name = data["category_name"]
+    category_id = Universale::DB.query_one("SELECT id FROM categories WHERE name = $1", [category_name], as: {id: Int32})[:id] rescue nil
+    category_id = Category.insert({name: category_name}) if category_id.nil?
+    record = {title: data["title"], data: data["data"], description: data["description"], category_id: category_id}
     success = Record.update({id: id}, record)
     if Universale::ApiJson.json?(env)
       record = Record.find(id)
