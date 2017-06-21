babab1bbc42fd4c2d6f35539af6c68d9f7c71827
Arthur Poulet <arthur.poulet@mailoo.org>
2017-06-21 17:03:21 +0100

    use category names

--- src/universale/webroot/records/create.cr
+++ src/universale/webroot/records/create.cr
@@ -1,7 +1,10 @@
 private def records_create(env)
   begin
     data = env.params.body
-    record = {title: data["title"], data: data["data"], description: data["description"], category_id: data["category_id"]}
+    category_name = data["category_name"]
+    category_id = Universale::DB.query_one("SELECT id FROM categories WHERE name = $1", [category_name], as: {id: Int32})[:id] rescue nil
+    category_id = Category.insert({name: category_name}) if category_id.nil?
+    record = {title: data["title"], data: data["data"], description: data["description"], category_id: category_id}
     id = Record.insert(record)
     if Universale::ApiJson.json?(env)
       record = Record.find(id)
