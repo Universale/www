d1a78463faa17b55a016abafb7b2540564c66f89
Arthur Poulet <arthur.poulet@mailoo.org>
2017-06-21 16:48:21 +0100

    Add category links

--- src/universale/webroot/index.cr
+++ src/universale/webroot/index.cr
@@ -1,4 +1,5 @@
 get "/" do
   recents = Universale::DB.query_all("SELECT title, data, description FROM records ORDER BY created_at DESC LIMIT 10;", as: {title: String, data: String, description: String})
+  categories = Category.all
   render "src/universale/webroot/index.slang", "src/universale/webroot/layouts/layout.slang"
 end
